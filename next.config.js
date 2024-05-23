587a24a90844bdd72a042a58e3a9f59e1021727f
YourEarth2411 <bekhanhcute2001@gmail.com>
2024-05-23 15:05:32 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production.up.railway.app",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "digitalproduct-production.up.railway.app",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
